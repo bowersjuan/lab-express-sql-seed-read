d380993efc2cb695ab9fd19df1aed2a4a0d0bd4e
bowersjuan <bowersjuan@gmail.com>
2023-02-23 13:04:25 -0500

    update seed.sql for typo

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 \c songs_dev;
 
 INSERT INTO songs ( name, artist, album, time, is_favorite ) VALUES 
-('Naima', 'John Coltrane', 'Giant Steps', "4:19", TRUE),
+('Naima', 'John Coltrane', 'Giant Steps', '4:19', TRUE),
 ('If I Were a Bell', 'Miles Davis', 'Relaxin with the Miles Davis Quintet', '8:18', TRUE),
 ('Night in Tunisia', 'Jesus Molina', 'Single', '3:37', FALSE);
