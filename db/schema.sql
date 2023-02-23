9b8261c58d30a0e355767ee73589904e70fd1efc
bowersjuan <bowersjuan@gmail.com>
2023-02-23 12:54:59 -0500

    Create squema.sql seed.sql and config.js

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE If EXISTS songs_dev;
+CREATE DATABASE songs_dev;
+
+\c songs_dev;
+
+CREATE TABLE song (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    album TEXT,
+    time TEXT,
+    is_favorite BOOLEAN
+);
